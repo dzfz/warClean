43676fd22f4760645c5227a56e65cc5fde44e417
定之方中 <kgdpgk@qq.com>
2016-02-16 14:30:15 +0800

    init

---
+++ warClean/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace warClean
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.warPath_button = new System.Windows.Forms.Button();
+            this.warPath_textBox = new System.Windows.Forms.TextBox();
+            this.run_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // warPath_button
+            // 
+            this.warPath_button.Location = new System.Drawing.Point(197, 12);
+            this.warPath_button.Name = "warPath_button";
+            this.warPath_button.Size = new System.Drawing.Size(75, 23);
+            this.warPath_button.TabIndex = 0;
+            this.warPath_button.Text = "war包路径";
+            this.warPath_button.UseVisualStyleBackColor = true;
+            this.warPath_button.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // warPath_textBox
+            // 
+            this.warPath_textBox.Location = new System.Drawing.Point(12, 12);
+            this.warPath_textBox.Name = "warPath_textBox";
+            this.warPath_textBox.Size = new System.Drawing.Size(168, 21);
+            this.warPath_textBox.TabIndex = 1;
+            // 
+            // run_button
+            // 
+            this.run_button.Location = new System.Drawing.Point(100, 49);
+            this.run_button.Name = "run_button";
+            this.run_button.Size = new System.Drawing.Size(75, 23);
+            this.run_button.TabIndex = 2;
+            this.run_button.Text = "清理";
+            this.run_button.UseVisualStyleBackColor = true;
+            this.run_button.Click += new System.EventHandler(this.run_button_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.run_button);
+            this.Controls.Add(this.warPath_textBox);
+            this.Controls.Add(this.warPath_button);
+            this.Name = "MainForm";
+            this.Text = "war包清理";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button warPath_button;
+        private System.Windows.Forms.TextBox warPath_textBox;
+        private System.Windows.Forms.Button run_button;
+
+    }
+}
+
---
+++ warClean/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Compression;
+
+namespace warClean
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //MessageBox.Show("load");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show("1");
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.ShowDialog();
+            this.warPath_textBox.Text = fbd.SelectedPath;
+        }
+
+        private void run_button_Click(object sender, EventArgs e)
+        {
+            string warPath = this.warPath_textBox.Text;
+            if (warPath != "" && warPath != null)
+            {
+                cleanWarFile(warPath);
+                MessageBox.Show("清理成功！");
+            }
+            else
+            {
+                MessageBox.Show("war包路径为空,请选择路径。");
+            }
+
+        }
+
+        private void cleanWarFile(string warPath)
+        {
+            string[] fileArray = Directory.GetFiles(warPath, "*.war");
+            foreach (string file in fileArray)
+            {
+                string extractPath = warPath + "\\war_temp";
+                if (Directory.Exists(extractPath))
+                {
+                    try
+                    {
+                        Directory.Delete(extractPath);
+                    }catch()
+                    {
+                    }
+
+                }
+                ZipFile.ExtractToDirectory(file, extractPath);
+                if (Directory.Exists(extractPath))
+                {
+                    DirectoryInfo extractPathFolder = new DirectoryInfo(extractPath);
+                    foreach (DirectoryInfo nextFolder in extractPathFolder.GetDirectories())
+                    {
+                        Console.WriteLine(nextFolder.Name);
+                    }
+                    foreach (FileInfo nextFile in extractPathFolder.GetFiles())
+                    {
+
+                    }
+                }
+                //MessageBox.Show(returnVal.ToString());
+            }
+
+        }
+
+
+
+
+    }
+}
